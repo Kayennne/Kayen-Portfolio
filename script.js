9d430c605271ebca9ed9e0518ffd83da1d345292
Kayennne <116442544+Kayennne@users.noreply.github.com>
2024-03-31 02:21:25 +0800

    done slideshow for project page
    took me a long 4 hours because i cant do slideshows :'(

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.right = '-250px'
 }
+
+// let list = document.querySelector('.slider .list');
+// let items = document.querySelectorAll('.slider .list .item');
+// let dots = document.querySelectorAll ('.slider .dots li');
+
+// let active = 0;
+// let lengthItems = items.length - 1;
+
+// function reloadSlider(){
+//     let checkLeft = items[active].offsetLeft;
+//     list.style.left = -checkLeft + 'px';
+
+//     let lastActiveDot = document.querySelector('.slider .dots li.active');
+//     lastActiveDot.classList.remove('active');
+//     dots[active].classList.add('active');
+// }
+
+// dots.forEach((li, key) => {
+//     li.addEventListener('click', function(){
+//         active = key;
+//         reloadSlider();
+//     })
+// })
+
